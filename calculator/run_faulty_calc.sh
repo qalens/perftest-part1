2d50f5bef176eee3618b4f265a81eaeeb77f972b
ATM <naik.atmaram@gmail.com>
2022-06-12 16:52:37 +0530

    ATM | modified k6 scripts

--- calculator/run_faulty_calc.sh
+++ calculator/run_faulty_calc.sh
@@ -1,3 +1,3 @@
 docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker run -p 8020:8020 -e VERSION=faulty_calc --memory 250m --memory-swap 250m -d --name calc  atmaram/calc
+docker run -p 8020:8020 -e VERSION=faulty_calc --memory 200m --memory-swap 200m -d --name calc  atmaram/calc
